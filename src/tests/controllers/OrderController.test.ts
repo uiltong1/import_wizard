310f0d893b4e802ee9c8ac6bca73c70cd1cb7f9b
Uilton Gomes <uilton.gomes@acpgroup.com.br>
2024-09-20 23:10:27 -0300

    test: adição de testes no OrderController

---
+++ src/tests/controllers/OrderController.test.ts
@@ -0,0 +1,118 @@
+import { Request, Response } from 'express';
+import { OrderController } from '../../controllers/OrderController';
+import { OrderService } from '../../services/OrderService';
+import { PaginatedOrdersDTO } from '../../DTO/PaginatedOrdersDTO';
+
+describe('OrderController', () => {
+    let orderController: OrderController;
+    let orderServiceMock: jest.Mocked<OrderService>;
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+
+    beforeEach(() => {
+        orderServiceMock = {
+            getOrders: jest.fn(),
+            importOrders: jest.fn(),
+        } as unknown as jest.Mocked<OrderService>;
+
+        orderController = new OrderController(orderServiceMock);
+
+        req = {
+            query: {},
+        };
+        res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+        };
+    });
+
+    describe('getOrders', () => {
+        it('deve retornar pedidos com sucesso', async () => {
+            const mockResult: PaginatedOrdersDTO = {
+                users: [],
+                total: 0,
+                page: 1,
+                totalPages: 1,
+            };
+
+            orderServiceMock.getOrders.mockResolvedValue(mockResult);
+
+            req.query = {
+                orderId: '1',
+                startDate: '2023-01-01',
+                endDate: '2023-12-31',
+                page: '1',
+                limit: '10',
+            };
+
+            await orderController.getOrders(req as Request, res as Response);
+
+            expect(res.json).toHaveBeenCalledWith(mockResult);
+        });
+
+        it('deve retornar erro 500 em caso de exceção', async () => {
+            orderServiceMock.getOrders.mockRejectedValue(new Error('Erro ao obter pedidos'));
+
+            await orderController.getOrders(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith({ error: 'Erro ao obter pedidos' });
+        });
+    });
+
+    describe('importOrders', () => {
+        it('deve retornar erro 400 se o arquivo não for fornecido', async () => {
+            await orderController.importOrders(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: 'Arquivo não fornecido' });
+        });
+
+        it('deve importar pedidos com sucesso', async () => {
+            req.file = {
+                path: 'fake/path.txt',
+                fieldname: 'file',
+                originalname: 'orders.txt',
+                encoding: '7bit',
+                mimetype: 'text/plain',
+                size: 1024,
+                destination: 'uploads/',
+                filename: 'orders.txt',
+                buffer: Buffer.from(''),
+            } as any;
+
+            orderServiceMock.importOrders.mockResolvedValue();
+
+            await orderController.importOrders(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(201);
+            expect(res.json).toHaveBeenCalledWith({
+                message: 'Pedidos importados com sucesso!'
+            });
+        });
+
+        it('deve retornar erro 500 em caso de exceção durante a importação', async () => {
+            req.file = {
+                path: 'fake/path.txt',
+                fieldname: 'file',
+                originalname: 'orders.txt',
+                encoding: '7bit',
+                mimetype: 'text/plain',
+                size: 1024,
+                destination: 'uploads/',
+                filename: 'orders.txt',
+                buffer: Buffer.from(''),
+            } as any;
+
+            orderServiceMock.importOrders.mockRejectedValue(new Error('Erro ao importar pedidos'));
+
+            await orderController.importOrders(req as Request, res as Response);
+
+            expect(res.status).toHaveBeenCalledWith(500);
+            expect(res.json).toHaveBeenCalledWith({
+                message: 'Erro ao importar pedidos',
+                error: 'Erro ao importar pedidos',
+            });
+        });
+    });
+});
