44fb07cefc8d2d0e1924ea14a386f32e017b417a
Uilton Gomes <uilton.gomes@acpgroup.com.br>
2024-09-22 11:39:24 -0300

    style: ajuste no layout do sistema e pagina de pedidos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <router-view />
+  <q-layout view="lHh lpr lFf" container class="shadow-2 rounded-borders">
+    <q-header bordered class="bg-grey-3 text-primary">
+      <q-toolbar>
+        <q-toolbar-title class="text-center"> Import Wizard </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-footer bordered class="bg-grey-3 text-primary text-center">
+      Desenvolvido por Uilton Gomes
+    </q-footer>
+    <q-page-container style="height: 100%">
+      <q-page class="q-pa-md" style="height: 100%">
+        <router-view />
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script setup lang="ts">
 defineOptions({
-  name: 'App'
+  name: 'App',
 });
 </script>
--- frontend/src/modules/order/pages/Index.vue
+++ frontend/src/modules/order/pages/Index.vue
@@ -1,14 +1,30 @@
 <template>
-    <div>
-      <h1>Página de Pedidos</h1>
-      <!-- Conteúdo da página -->
+  <div class="q-pa-md">
+    <div class="row">
+      <div class="col-md-12 q-pb-xl">
+        <span class="title-component"> Pedidos </span>
+        <q-separator inset class="full-width" />
+      </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-    defineOptions({
-    name: 'IndexOrder'
-  });
+    <div class="row justify-between q-pb-xl">
+      <div class="col-md-4">
+        <q-input filled label="Pesquisar aqui...">
+          <template #append>
+            <q-icon name="search" class="cursor-pointer" />
+          </template>
+        </q-input>
+      </div>
+      <div class="col-md-4 offset-md-4">
+        <div class="column items-end">
+          <q-btn label="Criar" color="secondary" icon="add"></q-btn>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-  </script>
-  
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexOrder',
+});
+</script>
