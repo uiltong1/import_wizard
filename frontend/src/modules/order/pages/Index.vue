03f5023afc4d14cc38e27492065be6f29f1d40e8
Uilton Gomes <uilton.gomes@acpgroup.com.br>
2024-09-22 13:14:57 -0300

    style: ajusta layout

--- frontend/src/modules/order/pages/Index.vue
+++ frontend/src/modules/order/pages/Index.vue
@@ -6,17 +6,43 @@
         <q-separator inset class="full-width" />
       </div>
     </div>
-    <div class="row justify-between q-pb-xl">
-      <div class="col-md-4">
-        <q-input filled label="Pesquisar aqui...">
+    <div class="row justify-center">
+      <div class="col-md-3 q-mb-md q-mx-md">
+        <q-input
+          filled
+          label="Número do Pedido"
+          type="number"
+          v-model="orderId"
+        >
           <template #append>
             <q-icon name="search" class="cursor-pointer" />
           </template>
         </q-input>
       </div>
-      <div class="col-md-4 offset-md-4">
-        <div class="column items-end">
-          <q-btn label="Criar" color="secondary" icon="add"></q-btn>
+      <div class="col-md-4 q-mb-md q-mx-md">
+        <q-input filled type="date" label="Data Inicial" v-model="startDate">
+          <template #append>
+            <q-icon name="search" class="cursor-pointer" />
+          </template>
+        </q-input>
+      </div>
+      <div class="col-md-4 q-mb-md q-mx-md">
+        <q-input filled type="date" label="Data Final" v-model="endDate">
+          <template #append>
+            <q-icon name="search" class="cursor-pointer" />
+          </template>
+        </q-input>
+      </div>
+    </div>
+    <div class="row-rever">
+      <div class="col-md-2 q-mb-md q-mx-md">
+        <div class="column">
+          <q-btn
+            label="Importar Pedidos"
+            color="secondary"
+            icon="add"
+            @click="handleCreate"
+          />
         </div>
       </div>
     </div>
@@ -38,11 +64,22 @@ defineOptions({
 });
 
 const searchQuery = ref<string>('');
+const orderId = ref<number | null>(null);
+const startDate = ref<string | null>(null);
+const endDate = ref<string | null>(null);
+
+const handleCreate = () => {
+  console.log('asdasda');
+};
+
 const store = useStore();
 
 const loadOrders = () => {
   console.log(searchQuery.value);
   const params: FetchOrdersParams = {
+    /* startDate: '2021-11-23',
+    endDate: '2021-11-27',
+    orderId: 146, */
     page: 5,
     limit: 90,
   };
